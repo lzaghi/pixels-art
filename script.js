172a5baad5e2901afa939f22a3461487ab276307
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-02 15:52:03 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+localStorage.setItem('colorBlack', 'selected')
+
 const botaoPaleta = document.getElementsByClassName('color')
 
 function defineClasse(event) {
@@ -12,3 +14,27 @@ function defineClasse(event) {
 for (let i = 0; i < botaoPaleta.length; i += 1) {
   botaoPaleta[i].addEventListener('click', defineClasse)
 }
+
+const botaoPixel = document.getElementsByClassName('pixel')
+
+function colocaCor(event) {
+  let corSelecionada = document.getElementsByClassName('color selected')[0].innerHTML
+  console.log(corSelecionada);
+  event.target.style.backgroundColor = corSelecionada;
+}
+
+for (let j = 0; j < botaoPixel.length; j += 1) {
+  botaoPixel[j].addEventListener('click', colocaCor)
+}
+
+// let corSelecionada = document.getElementsByClassName('selected')
+// console.log(corSelecionada);
+
+function selectedBlack() {
+  let classeSelected = localStorage.getItem('colorBlack');
+  let paletaPreta = document.getElementsByClassName('color')[0];
+
+  paletaPreta.classList.add(classeSelected)
+}
+
+window.onload = selectedBlack();
